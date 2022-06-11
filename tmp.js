65ca37cb13b9aa2cae820cb50b7cd35d577132ea
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-06-10 23:27:04 -0400

    updating intro

--- tmp.js
+++ tmp.js
@@ -342,6 +342,7 @@ const Config = {
         const element = await waitForSelectors([["aria/Dashboard"],["#breadcrumbs > li:nth-child(1) > a"]], targetPage, { timeout, visible: true });
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 51.8046875, y: 25} });
+        await targetPage.waitForNavigation({waitUntil: 'networkidle2'})
         //await Promise.all(promises);
     }
 
